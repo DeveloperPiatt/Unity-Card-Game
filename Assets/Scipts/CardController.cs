8a5e31be97f59a0fa8fae65c1eb1b7a4af563d00
DeveloperPiatt <developer.piatt@gmail.com>
2015-03-25 00:29:31 -0700

    First Commit
    https://www.youtube.com/watch?v=D0GOgSkHahI
    
    Part 1 completed

---
+++ Assets/Scipts/CardController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardController : MonoBehaviour {
+
+	public int cardIndex;
+	public Texture[] faces;
+	public Texture background;
+	public Texture outline;
+
+	public void ShowBackground() {
+		GetComponent<Renderer> ().enabled = true;
+		GetComponent<Renderer> ().material.mainTexture = background;
+		//renderer.material.mainTexture = background;
+	}
+
+	public void HideCard() { 
+		GetComponent<Renderer> ().enabled = false;
+	}
+
+	public void ShowFace() {
+		GetComponent<Renderer> ().enabled = true;
+		GetComponent<Renderer> ().material.mainTexture = faces[cardIndex];
+	}
+
+	public void ShowCardOutline() {
+		GetComponent<Renderer> ().enabled = true;
+		GetComponent<Renderer> ().material.mainTexture = outline;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scipts/DebugCardBehavior.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugCardBehavior : MonoBehaviour {
+
+	CardController cardController;
+
+	int state = 0;
+	// Use this for initialization
+	void Start () {
+		cardController = GetComponent<CardController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUp() {
+		state++;
+
+		switch (state) {
+		case 1:
+			cardController.ShowBackground();
+			break;
+		case 2:
+			cardController.ShowFace();
+			break;
+		case 3:
+			cardController.HideCard();
+			break;
+		case 4:
+			cardController.ShowCardOutline();
+			state = 0;
+			break;
+		}
+	}
+}
