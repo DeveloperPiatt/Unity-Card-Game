61556ae7e260df7048ca18588639396ddbdaaeb2
DeveloperPiatt <developer.piatt@gmail.com>
2015-03-25 19:25:51 -0700

    Added card hand
    Added card hand and finish second part of tutorial.

---
+++ Assets/Scipts/GameTableController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTableController : MonoBehaviour {
+
+	//TODO: Build a deck of cards
+	//TODO: Deal out 5 cards to the player
+	//TODO: Take the 5 cards out of the deck
+	//TODO: Display the 5 cards for the player
+
+	public GameObject playerHand;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scipts/HandController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandController : MonoBehaviour {
+
+	public GameObject[] cards;
+	public int clientID;
+
+	// Use this for initialization
+
+	public void AddCard (int cardId, int cardIndex, bool showFaceUp){
+		CardController cardController = cards [cardId].GetComponent<CardController> ();
+		cardController.cardIndex = cardIndex;
+		if (showFaceUp) {
+			cardController.ShowFace ();
+		} else {
+			cardController.ShowBackground();
+		}
+	}	
+
+	public void Reset() {
+
+		CardController firstCardController = cards [0].GetComponent<CardController> ();
+		firstCardController.cardIndex = 0;
+		firstCardController.ShowCardOutline();
+
+		for (int x = 1; x < cards.Length; x++) {
+			CardController cardController = cards[x].GetComponent<CardController>();
+			cardController.HideCard();
+		}
+	}
+
+	void Start () {
+	
+		Reset ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
