6b7292d40b1764fcfd1b613ff00c6da45922a959
DeveloperPiatt <developer.piatt@gmail.com>
2015-03-25 20:44:22 -0700

    Adding deck
    Adding a deck to the game

---
+++ Assets/Scipts/DeckController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeckController : MonoBehaviour {
+
+	public GameObject[] cards;
+
+	public void Shuffle() {
+
+		CardController aCard = cards [0].GetComponent<CardController> ();
+		int facesInt = aCard.faces.Length;
+
+		int[] randomFaces;
+		randomFaces = new int[facesInt];
+
+		for (int x = 0; x<facesInt; x++) {
+			randomFaces[x] = x;
+		}
+
+		for (int x = 0; x<cards.Length; x++) {
+			CardController thisCard = cards[x].GetComponent<CardController> ();
+			thisCard.cardIndex = randomFaces[x];
+			thisCard.ShowFace();
+
+		}
+		print ("Assigned all cards in deck an index");
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		Shuffle ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scipts/GameTableController.cs
+++ Assets/Scipts/GameTableController.cs
@@ -9,11 +9,25 @@ public class GameTableController : MonoBehaviour {
 	//TODO: Display the 5 cards for the player
 
 	public GameObject playerHand;
+	public GameObject playerCard;
+	public GameObject gameDeck;
 
+	private int deckSize;
+	private CardController[] deck;
+
+	public void ShuffleDeck(){
+
+		//deck has 8 cards right now
+		//find the length of a card's faces array
+
+		deckSize = playerCard.GetComponent<CardController> ().faces.Length;
+		print ("Shuffling " + deckSize + " cards!");
+
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		//ShuffleDeck ();
 	}
 	
 	// Update is called once per frame
