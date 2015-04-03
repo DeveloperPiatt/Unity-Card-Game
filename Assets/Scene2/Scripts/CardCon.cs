1e54abc02109ec72434eaa81027efa4039b7f6f8
DeveloperPiatt <developer.piatt@gmail.com>
2015-04-02 22:51:37 -0700

    hate life
    started up scene 2

---
+++ Assets/Scene2/Scripts/CardCon.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CardCon : MonoBehaviour {
+
+	public int cardIndex;
+	public Sprite[] faces;
+	public Sprite background;
+	public Sprite outline;
+	
+	public void ShowBackground() {
+		
+		// Make sure the card is visible and displaying the background texture
+
+		GetComponent<Image> ().enabled = true;
+		GetComponent<Image> ().sprite = background;
+		//GetComponent<Renderer> ().enabled = true;
+		//GetComponent<Renderer> ().material.mainTexture = background;
+	}
+	
+	public void HideCard() { 
+		
+		// Hiding the card from view
+		GetComponent<Image> ().enabled = false;
+		//GetComponent<Renderer> ().enabled = false;
+	}
+	
+	public void ShowFace() {
+		
+		// Make sure the card is visible and displaying the face corresponding to the cardIndex
+		GetComponent<Image> ().enabled = true;
+		GetComponent<Image> ().sprite = faces[cardIndex];
+	}
+	
+	public void ShowCardOutline() {
+		
+		// Mark sure the card is visible and displaying the card outline
+		GetComponent<Image> ().enabled = true;
+		GetComponent<Image> ().sprite = outline;
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scipts/DragController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DragController : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
+
+	private Vector3 handPosition;
+
+	public void OnBeginDrag (PointerEventData eventData) {
+		Debug.Log ("OnBeginDrag");
+		//handPosition = this.transform.position;
+	}
+
+	public void OnDrag (PointerEventData eventData) {
+		Debug.Log ("OnDrag");
+		this.transform.position = eventData.position;
+	}
+
+	public void OnEndDrag (PointerEventData eventData) {
+		Debug.Log ("OnEndDrag");
+		this.transform.position = handPosition;
+	}
+
+	void Start () {
+		handPosition = this.transform.position;
+	}
+
+}
