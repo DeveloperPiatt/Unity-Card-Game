5fe5f6f9bac070ad5508eadbcaca9b9da4110dc7
DeveloperPiatt <developer.piatt@gmail.com>
2015-04-03 20:47:15 -0700

    slow progress

--- Assets/Scipts/DragController.cs
+++ Assets/Scipts/DragController.cs
@@ -6,23 +6,27 @@ public class DragController : MonoBehaviour, IDragHandler, IBeginDragHandler, IE
 
 	private Vector3 handPosition;
 
+	Transform parentToReturnTo = null;
+
 	public void OnBeginDrag (PointerEventData eventData) {
 		Debug.Log ("OnBeginDrag");
-		//handPosition = this.transform.position;
+		parentToReturnTo = this.transform.parent;
+		//this.transform.SetParent (this.transform.parent.parent);
 	}
 
 	public void OnDrag (PointerEventData eventData) {
-		Debug.Log ("OnDrag");
+		//Debug.Log ("OnDrag");
 		this.transform.position = eventData.position;
 	}
 
 	public void OnEndDrag (PointerEventData eventData) {
 		Debug.Log ("OnEndDrag");
-		this.transform.position = handPosition;
+		//this.transform.SetParent (parentToReturnTo);
+		//this.transform.position = handPosition;
 	}
 
 	void Start () {
-		handPosition = this.transform.position;
+		//handPosition = this.transform.position;
 	}
 
 }
