65c7ea6bb8ca7852357cdc6ee3aa8333b1f66237
DeveloperPiatt <developer.piatt@gmail.com>
2015-04-16 14:28:10 -0700

    end of day

--- Assets/Scipts/CardController.cs
+++ Assets/Scipts/CardController.cs
@@ -6,16 +6,35 @@ public class CardController : MonoBehaviour {
 
 	public HandController.PlayerNumber playerNum;
 
+	public Text topValue;
+	public Text rightValue;
+	public Text downValue;
+	public Text leftValue;
+
 	void setPlayer() {
 		HandController cardHand = this.transform.parent.parent.GetComponent<HandController> ();
 		playerNum = cardHand.playerNum;
+
+		//setting background color to match player
+		//this.GetComponent<Image> ().color = new Color32 (0, 0, 0);
+	}
+
+
+	void setRandomValues() {
+
+		topValue.text = Random.Range (1, 9).ToString ();
+		rightValue.text = Random.Range (1, 9).ToString ();
+		downValue.text = Random.Range (1, 9).ToString ();
+		leftValue.text = Random.Range (1, 9).ToString ();
+
 	}
 
 	// Use this for initialization
 	void Start () {
 		setPlayer ();
+		setRandomValues ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
